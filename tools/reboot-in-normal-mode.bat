375d48c9b0d721e52ab86761f087a4a03869391f
WinTweaks <71241590+WinTweaks@users.noreply.github.com>
2020-09-21 11:07:50 -0400

    Create reboot-in-normal-mode.bat

---
+++ tools/reboot-in-normal-mode.bat
@@ -0,0 +1,20 @@
+:: REBOOT IN NORMAL MODE
+:: https://github.com/WinTweaks/tools-for-tweaking
+
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+:: CHECK FOR ADMIN PRIVILEGES
+whoami /groups | find "12288" >nul 2>&1
+if %errorlevel% neq 0 (
+	echo This batch file must be Run as Administrator.
+	pause
+	exit /b 1
+)
+
+:: CHANGE BOOT SETTINGS
+bcdedit /deletevalue safebootalternateshell >nul 2>&1
+bcdedit /deletevalue safeboot >nul 2>&1
+
+:: RESTART
+shutdown /r /f /t 5
