79aee0373b58b97d3da7d2d8609873a256f45d2f
WinTweaks <71241590+WinTweaks@users.noreply.github.com>
2020-09-21 11:09:18 -0400

    added command-line tools

---
+++ tools/command-line-tools/README.md
@@ -0,0 +1,23 @@
+# Windows 10 Command Line Tools
+
+All of these batch scripts and executables should be installed in the **C:\Windows** folder for proper execution. Each is meant to be executed from the command line with command line arguments. 
+
+Installation:
+
+Copy to the *C:\Windows* folder.
+
+Usage:
+
+*command* [option]
+
+## Disclaimer
+
+These scripts have been tested on 64-bit Windows 10. They make or may not work on other versions of Windows. 
+
+- This GitHub is not tech support.
+- Use Google before asking questions.
+- Never use a script you don't understand.
+- Backup your computer before running a script.
+- I am not responsible if you break your PC.
+
+>THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tools/command-line-tools/reboot.bat
@@ -0,0 +1,68 @@
+:: REBOOT SYSTEM
+:: Usage: reboot [ /c | /m | /n | /s ]
+:: https://github.com/WinTweaks/tools-for-tweaking
+
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+:: CHECK FOR ADMIN PRIVILEGES
+whoami /groups | find "12288" >nul 2>&1
+if %errorlevel% neq 0 (
+	echo This batch file must be Run as Administrator.
+	pause
+	exit /b 1
+)
+
+bcdedit /deletevalue safebootalternateshell >nul 2>&1
+
+if "%1"=="/?" goto :Help
+
+if "%1"=="console" goto :Console
+if "%1"=="/console" goto :Console
+if "%1"=="/c" goto :Console
+
+if "%1"=="minimal" goto :Minimal
+if "%1"=="/minimal" goto :Minimal
+if "%1"=="/m" goto :Minimal
+
+if "%1"=="network" goto :SafeMode
+if "%1"=="/network" goto :SafeMode
+if "%1"=="safemode" goto :SafeMode
+if "%1"=="/safemode" goto :SafeMode
+if "%1"=="/n" goto :SafeMode
+if "%1"=="/s" goto :SafeMode
+
+:: Reboot in Normal Mode
+echo Default
+exit /b 0
+
+:: Reboot in Safe Mode with Command Prompt
+:Console
+echo Console
+goto :eof
+
+:: Reboot in Safe Mode
+:Minimal
+echo Minimal
+goto :eof
+
+:: Reboot in Safe Mode with Networking
+:SafeMode
+echo SafeMode
+goto :eof
+
+:: Help
+:Help
+echo Reboot the system.
+echo.
+echo USAGE:
+echo     reboot [option]
+echo.
+echo     Options:
+echo         No args           Reboot in normal mode.
+echo.        /?                Display this help message.
+echo         /c, /console      Reboot in safe mode with command prompt.
+echo         /m, /minimal      Reboot in safe mode.
+echo         /n, /network      Reboot in safe mode with networking.
+echo         /s, /safemode     Reboot in safe mode with networking.
+goto :eof
