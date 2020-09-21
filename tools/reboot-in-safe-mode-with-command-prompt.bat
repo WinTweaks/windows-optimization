aa2be917191877e6337d8b9b801b414854dd06c8
WinTweaks <71241590+WinTweaks@users.noreply.github.com>
2020-09-21 11:08:04 -0400

    Create reboot-in-safe-mode-with-command-prompt.bat

---
+++ tools/reboot-in-safe-mode-with-command-prompt.bat
@@ -0,0 +1,20 @@
+:: REBOOT IN SAFE MODE WITH COMMAND PROMPT
+:: https://github.com/WinTweaks/tools-for-tweaking
+
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+:: CHECK FOR ADMIN PRIVILEGES
+whoami /groups | find "12288" >nul 2>&1
+if %errorlevel% neq 0 (
+	echo This batch file must be Run as Administrator.
+	pause
+	exit /b 1
+)
+
+:: CHANGE BOOT SETTINGS
+bcdedit /set safeboot minimal >nul 2>&1
+bcdedit /set safebootalternateshell yes >nul 2>&1
+
+:: RESTART
+shutdown /r /f /t 5
