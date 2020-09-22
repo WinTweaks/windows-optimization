b2df246d0ec61b48ec59232f31f868a2d1a96f84
WinTweaks <71241590+WinTweaks@users.noreply.github.com>
2020-09-22 11:17:30 -0400

    Update restart-network.bat

--- network/restart-network.bat
+++ network/restart-network.bat
@@ -7,8 +7,8 @@ setlocal ENABLEDELAYEDEXPANSION
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
 
-:: RESTART EACH CONNECTED INTERFACE
+:: RESTART EACH CONNECTED NETWORK ADAPTER
 for /f "tokens=3,*" %%i in ('netsh int show interface^|find "Connected"') do (
-	echo %%j
-	netsh int set interface name="%%j" admin="disabled" && netsh int set interface name="%%j" admin="enabled" >nul 2>&1
+	netsh int set interface name="%%j" admin="disabled" >nul 2>&1
+	netsh int set interface name="%%j" admin="enabled" >nul 2>&1
 )
