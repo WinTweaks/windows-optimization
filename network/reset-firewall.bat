b2a6cce44349d8f977d6c7022bfb93c004e9270f
Phlegm <71241590+WinTweaks@users.noreply.github.com>
2020-09-22 23:52:58 -0400

    Update reset-firewall.bat

--- network/reset-firewall.bat
+++ network/reset-firewall.bat
@@ -7,6 +7,11 @@ setlocal ENABLEDELAYEDEXPANSION
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
 
-netsh advfirewall set allprofiles state off
-netsh advfirewall reset
-netsh advfirewall set allprofiles state on
+netsh advfirewall set allprofiles state off >nul 2>&1
+netsh advfirewall firewall delete rule all
+netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound >nul 2>&1
+netsh advfirewall reset >nul 2>&1
+netsh advfirewall set allprofiles state on >nul 2>&1
+
+exit /b 0
+
