64f493bb1953967322d97c3487ae30ac92200acb
Phlegm <71241590+WinTweaks@users.noreply.github.com>
2020-09-29 23:55:58 -0400

    Create reset-network-settings.bat

---
+++ network/reset-network-settings.bat
@@ -0,0 +1,14 @@
+:: RESTART NETWORK ADAPTER
+:: https://github.com/wintweaks/windows-optimization
+
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+:: CHECK FOR ADMIN PRIVILEGES
+dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
+
+:: RESET ALL NETWORK SETTINGS
+ipconfig /flushdns >nul 2>&1
+netsh int ipv4 reset >nul 2>&1
+netsh int ipv6 reset >nul 2>&1
+netsh winsock reset >nul 2>&1
